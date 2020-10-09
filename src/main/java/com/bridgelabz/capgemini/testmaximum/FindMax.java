99b5945748a7382586b52eb880675400e9193f02
ppgupta <premprakashnayak69@gmail.com>
2020-10-10 03:03:24 +0530

    max String

--- src/main/java/com/bridgelabz/capgemini/testmaximum/FindMax.java
+++ src/main/java/com/bridgelabz/capgemini/testmaximum/FindMax.java
@@ -4,28 +4,32 @@ package com.bridgelabz.capgemini.testmaximum;
  * Hello world!
  *
  */
-public class FindMax 
-{
+public class FindMax <E extends Comparable<E>>
+{ 
+	private E x,y,z;
 	public void printWelcome() {
 		System.out.println("Welcome");
 	}
     
-	public Integer  getMax(Integer x,Integer y,Integer z) {
-		Integer maximum=x;
-    	if(y.compareTo(maximum)>0)
-    		maximum=y;
-    	if(z.compareTo(maximum)>0)
-    		maximum=z;
-    	return maximum;
-    } 
-	public Double  getMax(Double x,Double y,Double z) {
-		Double maximum=x;
-    	if(y.compareTo(maximum)>0)
-    		maximum=y;
-    	if(z.compareTo(maximum)>0)
-    		maximum=z;
-    	return maximum;
-    } 
+	public FindMax(E x, E y, E z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public  E getMax() {
+		return getMax(x, y, z);
+	}
+
+	public static <E extends Comparable<E>> E getMax(E a, E b, E c) {
+		E max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
+	
 	
 	
 	
--- src/test/java/com/bridgelabz/capgemini/testmaximum/FindMaxTest.java
+++ src/test/java/com/bridgelabz/capgemini/testmaximum/FindMaxTest.java
@@ -9,40 +9,60 @@ public class FindMaxTest
 {
 	@Test
 	public void maximumIntegerAtPosition1() {
-		FindMax findMax = new FindMax();
-		int max = findMax.getMax(5, 4, 3);
+		FindMax<Integer> findMax = new FindMax<>(5, 4, 3);
+		int max = findMax.getMax();
 		assertEquals(5, max);
 	}
 
 	@Test
 	public void maximumIntegerAtPosition2() {
-		FindMax findMax = new FindMax();
+		FindMax<Integer> findMax = new FindMax<>(5, 9,4 );
 		int max = findMax.getMax(5, 9, 4);
 		assertEquals(9, max);
 	}
 
 	@Test
 	public void maximumIntegerAtPosition3() {
-		FindMax findMax = new FindMax();
+		FindMax<Integer> findMax = new FindMax<>(5, 4, 20);
 		int max = findMax.getMax(5, 4, 20);
 		assertEquals(20, max);
 	}
+	
 	@Test
-	public void maximumDoubleAtPosition1() {
-		FindMax findMax = new FindMax();
-		double max = findMax.getMax(5.333, 4.54, 3.52);
+	public void maximumFloatAtPosition1() {
+		FindMax<Double> findMax = new FindMax<>(5.333, 4.54, 3.52);
+		double max = findMax.getMax();
 		assertEquals(5.333, max,.001);
 	}
 	@Test
-	public void maximumDoubleAtPosition2() {
-		FindMax findMax = new FindMax();
-		double max = findMax.getMax(5.333, 14.54, 3.52);
+	public void maximumFloatAtPosition2() {
+		FindMax<Double> findMax = new FindMax<>(5.333, 14.54, 3.52);
+		double max = findMax.getMax();
 		assertEquals(14.54, max,.001);
 	}
 	@Test
-	public void maximumDoubleAtPosition3() {
-		FindMax findMax = new FindMax();
-		double max = findMax.getMax(5.333, 4.54, 13.52);
+	public void maximumFloatAtPosition3() {
+		FindMax<Double> findMax = new FindMax<>(5.333, 4.54, 13.52);
+		double max = findMax.getMax();
 		assertEquals(13.52, max,.001);
+	}
+	
+	@Test
+	public void maximumStringAtPosition1() {
+		FindMax<String> findMax = new FindMax<>("Peach","Apple", "Banana");
+		String max = findMax.getMax( );
+		assertEquals("Peach", max);
+	}
+	@Test
+	public void maximumStringAtPosition2() {
+		FindMax<String> findMax = new FindMax<>("Apple","Peach", "Banana");
+		String max = findMax.getMax();
+		assertEquals("Peach", max);
+	}
+	@Test
+	public void maximumStringAtPosition3() {
+		FindMax<String> findMax = new FindMax<>("Banana","Apple", "Peach");
+		String max = findMax.getMax();
+		assertEquals("Peach", max);
 	}
 }
