8dcf348e6c96d86d1b2dbf24373bb27e54bc9edf
ppgupta <premprakashnayak69@gmail.com>
2020-10-10 01:23:00 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/capgemini/testmaximum/FindMax.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.capgemini.testmaximum;
+
+/**
+ * Hello world!
+ *
+ */
+public class FindMax 
+{
+	public void printWelcome() {
+		System.out.println("Welcome");
+	}
+    
+}
---
+++ src/test/java/com/bridgelabz/capgemini/testmaximum/FindMaxTest.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.capgemini.testmaximum;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FindMaxTest 
+{
+    
+   
+}
